15a55a75ba190dad95d7cc017e3f57c84490c141
irvingxd <mskucas1@gmail.com>
2026-01-05 17:36:50 +0200

    modified:   pages/Dashboard.ts 	new file:   tests/test_dashboard_cookie_version.spec.ts

--- pages/Dashboard.ts
+++ pages/Dashboard.ts
@@ -8,6 +8,7 @@ export class DashboardPage {
   readonly usersLink: Locator;
   readonly nodesLink: Locator;
   readonly settingsLink: Locator;
+  readonly serversLink: Locator;
 
     constructor(page: Page) {
     this.page = page;
@@ -16,16 +17,27 @@ export class DashboardPage {
     this.usersLink = page.getByRole('link', { name: /Users/ });
     this.nodesLink = page.getByRole('link', { name: /Nodes/ });
     this.settingsLink = page.getByRole('link', { name: /Settings/ });
+    this.serversLink = page.getByRole('link', { name: /Servers/ }); // naujas selktorius
   }
     async goto() {
     await this.adminLink.click();
   }  
-
-
     async navigateToAdmin() {
-    await this.page.locator('a[href="/admin/databases"]').click();
+    await this.page.locator('a[href="/admin"]').click();
   }
   async navigateToUsers() {
     await this.usersLink.click();
-  }     
+  }
+  async navigateToDatabases() {
+    await this.databasesLink.click();
+  }
+  async navigateToNodes() {
+    await this.nodesLink.click();
+  }
+  async navigateToSettings() {
+    await this.settingsLink.click();
+  }
+  async navigateToServers() {
+    await this.serversLink.click();
+  }
 }
